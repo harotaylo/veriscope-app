4492c6e5c056e61336ddf3ac24ecf6cf0c5d6ce4
harotaylo <harotaylo1@gmail.com>
2026-06-06 01:48:25 -0400

    Update OfficialTracker.jsx

--- src/screens/OfficialTracker.jsx
+++ src/screens/OfficialTracker.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../lib/supabase';
 import '../styles/OfficialTracker.css';
 
 export default function OfficialTracker() {
